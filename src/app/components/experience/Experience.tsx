c1c0d47bf65305ea8a562d1a158307d7c9361a19
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-06-12 16:01:03 -0300

    Componente de experience

---
+++ src/app/components/experience/Experience.tsx
@@ -0,0 +1,17 @@
+import { SectionTitle } from "../sectionTitle/SectionTitle"
+import "./Experience.scss"
+
+export function Experience() {
+  return (
+    <div className="experience">
+      <SectionTitle text="Experience"/>
+      <p>
+        3 years working as a Software Developer, in companys such as Itaú, Stone
+        and Aftersale.
+      </p>
+      <div className="experience-time">
+
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,30 @@
+import { Experience } from "./components/experience/Experience";
 import { Header } from "./components/header/Header";
-import "./components/styles/home.scss"
+import "./components/styles/home.scss";
 
 export default function Home() {
   return (
     <main className="container">
-      <Header/>
-      <div className="experience">
-        <h3>Experience</h3>
-        <p>
-          3 years working as a Software Developer, in companys such as Itaú,
-          Stone and Aftersale.
-        </p>
-        <div className="experience-time"></div>
-
-        <div className="infos">
-          <h3>Languages</h3>
-          <div className="languages-info">
-            <span>🇺🇸 EN- Fluent</span>
-            <span>🇧🇷 PT-Br - Native Speaker</span>
-          </div>
-          <h3>Education</h3>
-          <div className="education-info">
-            <span>🎓</span>
-            <span>
-              Software Engineer Degree - UDESC - Universidade do Estado de Santa
-              Catarina
-            </span>
-          </div>
+      <Header />
+      <Experience/>
+      <div className="infos">
+        <h3>Languages</h3>
+        <div className="languages-info">
+          <span>🇺🇸 EN- Fluent</span>
+          <span>🇧🇷 PT-Br - Native Speaker</span>
         </div>
-        <div className="buttons">
-          <div className="social"></div>
-          <button>Contact me</button>
+        <h3>Education</h3>
+        <div className="education-info">
+          <span>🎓</span>
+          <span>
+            Software Engineer Degree - UDESC - Universidade do Estado de Santa
+            Catarina
+          </span>
         </div>
+      </div>
+      <div className="buttons">
+        <div className="social"></div>
+        <button>Contact me</button>
       </div>
     </main>
   );
