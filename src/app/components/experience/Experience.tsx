b70ce31707eedcec0256aafc57ce5cda8f6b3754
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-06-20 15:25:42 -0300

    Atualização experiencia

--- src/app/components/experience/Experience.tsx
+++ src/app/components/experience/Experience.tsx
@@ -6,11 +6,11 @@ import { Skill } from "../skill/skill";
 export function Experience() {
   return (
     <div className="experience">
-      <SectionTitle text="Experience" />
-      <p>
-        3 years working as a Software Developer, in companys such as Unidavi,
-        UDESC and Aftersale.
-      </p>
+      <SectionTitle text="*Experiências" />
+      <p>Unidavi - Programador Full Stack(2 Anos)</p>
+      <p>UDESC - Estágio (2 anos)</p>
+      <p>IPM Sistemas - Programador(2 Anos)</p>
+      <p> Casa do Computador- Técnico (2 Anos)</p>
       <div className="experience-time">
         <div>
           <Skill image="/react.png" measure={2} years="2 years" />
